4155128680c8abba82caf2e3468948a8b01a745a
Ravi S. Singh <ravi.ss@imerit.net>
2020-05-31 22:44:54 +0530

    Orbit around selected object added

--- src/helpers/tree.helper.js
+++ src/helpers/tree.helper.js
@@ -1,7 +1,42 @@
 import * as THREE from 'three';
 
-export const textureLoader = (url)=>{
+export const textureLoader = (url) => {
     const loader = new THREE.TextureLoader();
     const texture = loader.load(url);
     return texture;
+};
+
+export const addCubeWithTexture = (scene, size=1, { x, y, z }, url = 'FactLogo-innerCircle.svg') => {
+
+    const texture = textureLoader(url);
+    const material = new THREE.MeshLambertMaterial({ map: texture });
+    const geomatery = new THREE.BoxGeometry(size,size,size);
+    const cube = new THREE.Mesh(geomatery, material);
+    cube.position.set(x, y, z);
+    scene.add(cube);
+
+    // const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+    // const texture = textureLoader(url);
+    // const material = new THREE.MeshLambertMaterial({ map: texture });
+    // // const material = new THREE.MeshLambertMaterial({ color, wireframe: false, transparent: false, opacity: 0.85 });
+    // const cube = new THREE.Mesh(geometry, material);
+    // cube.position.set(x, y, z);
+    // cube.castShadow = true; //default is false
+    // cube.receiveShadow = true; //default is false
+    // scene.add(cube);
+    return cube;
+}
+
+export const addAmbianceLight = (scene, color) => {
+    // const light = new THREE.AmbientLight(0x87341c); // fact logo light
+    const light = new THREE.AmbientLight(color); // fact logo light
+    scene.add(light);
+    return light;
+};
+
+export const addDirectionalLight = (scene, color, intensity, { x, y, z }) => {
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(x, y, z).normalize();
+    scene.add(light);
+    return light;
 };
--- src/screens/CameraPosition/index.js
+++ src/screens/CameraPosition/index.js
@@ -1,37 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Layout from './../../components/Layout';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import $ from 'jquery';
-import { textureLoader } from './../../helpers/tree.helper';
+import { textureLoader, addCubeWithTexture, addAmbianceLight, addDirectionalLight } from './../../helpers/tree.helper';
 
-export default class CameraPosition extends Component{
-    constructor(props){
+export default class CameraPosition extends Component {
+    mouse = new THREE.Vector2();
+    INTERSECTED;
+    constructor(props) {
         super(props);
         this.containerRef = React.createRef();
+        this.canvasRef = React.createRef();
         this.width = 400;
         this.height = 400;
         // this.aspectRatio = this.width / this.height;
-        this.aspectRatio = 16/8;
+        this.aspectRatio = 16 / 8;
 
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, this.aspectRatio, 0.01, 10000);
-        
+
+
+        // this.raycaster = new THREE.Raycaster();
 
         this.animate = this.animate.bind(this);
 
 
     };
 
-    init(){
+    init() {
         this.canvas3d = $("#canvas3d").get(0);
         this.scene.background = new THREE.Color(0x000000);
         this.scene.add(this.camera);
 
-        const texture = textureLoader('FactLogo-innerCircle.svg');
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-        const geomatery = this.geomatery = new THREE.BoxGeometry(1, 1, 1);
-        this.cube = new THREE.Mesh(geomatery, material);
-        this.scene.add(this.cube);
+        // this.scene.add( new THREE.GridHelper( 1000, 10 ) );
+
+        this.cube = addCubeWithTexture(this.scene, 1, { x: 0, y: 0, z: 0 }, 'FactLogo-innerCircle.svg');
+        this.cube2 = addCubeWithTexture(this.scene, 1, { x: 2, y: 2, z: 2 }, 'riti.jpeg');
 
         const renderAttrs = {
             antialias: true,
@@ -43,44 +48,113 @@ export default class CameraPosition extends Component{
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(this.width, this.height);
 
-        this.camera.position.set(0, 0, 4);
+        // lights
+        addAmbianceLight(this.scene, 0x888888);
 
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+        this.camera.position.set(0, 0, 10);
+
+        this.setupRaycaster();
 
         this.animate();
+
+        this.canvasRef.current.addEventListener('mousemove', this.onDocumentMouseMove.bind(this), false);
+        this.canvasRef.current.addEventListener('dblclick', this.onDocumentDoubleClick.bind(this), false);
     }
 
-    onResize(){
-        console.log('resized');
-        
+    onDocumentMouseMove(event) {
+        event.preventDefault();
+        const { x, y } = this.getMousePos(this.canvasRef.current, event);
+        this.mouse.x = (x / this.width) * 2 - 1;
+        this.mouse.y = -(y / this.height) * 2 + 1;
     }
 
-    animate(){
+    onDocumentDoubleClick(event) {
+        // this.setOrbiterTarget(this.mouse.x,this.mouse.y,)
+        if (this.INTERSECTED) {
+            const { x, y, z } = this.INTERSECTED.position;
+            this.setOrbiterTarget(x,y,z);
+        }
+    }
+
+    getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        const x = evt.clientX - rect.left;
+        const y = evt.clientY - rect.top;
+        return { x, y };
+    }
+
+    setOrbiterTarget(x, y, z) {
+        if (this.controls) {
+            this.controls.target.set(x, y, z);
+        }
+    }
+
+    positionCamera(camera, { x, y, z }) {
+        camera.position.set(x, y, z);
+        camera.updateProjectionMatrix();
+    }
+
+    setupRaycaster() {
+        this.raycaster = new THREE.Raycaster();
+        this.raycaster.params.Points.threshold = .15;
+        this.raycaster.params.Line.threshold = .15;
+    }
+
+
+
+    animate() {
         this.updateCanvasSize();
         requestAnimationFrame(this.animate);
-        this.cube.rotation.y += 0.01;
+        // this.mouse.x=0;
+        // this.mouse.y=0;
+        this.raycaster.setFromCamera(this.mouse, this.camera);
+
+        var intersects = this.raycaster.intersectObjects(this.scene.children);
+        console.log(intersects);
+
+        if (intersects.length > 0) {
+            if (this.INTERSECTED != intersects[0].object) {
+                console.log(this.INTERSECTED);
+                if (this.INTERSECTED) this.INTERSECTED.material.emissive.setHex(this.INTERSECTED.currentHex);
+                this.INTERSECTED = intersects[0].object;
+                this.INTERSECTED.currentHex = this.INTERSECTED.material.emissive.getHex();
+                this.INTERSECTED.material.emissive.setHex(0x87341c);
+            }
+        } else {
+            if (this.INTERSECTED) this.INTERSECTED.material.emissive.setHex(this.INTERSECTED.currentHex);
+            this.INTERSECTED = null;
+        }
+
+        if (this.INTERSECTED) {
+            // this.INTERSECTED.rotation.z += 0.1;
+        }
+        this.controls.update();
+        this.camera.updateMatrix();
         this.renderer.render(this.scene, this.camera);
     }
 
-    updateCanvasSize(){
-        if(this.containerRef?.current){
+    updateCanvasSize() {
+        if (this.containerRef?.current) {
             this.width = this.containerRef.current.offsetWidth;
-            this.height = this.width/this.aspectRatio;
+            this.height = this.width / this.aspectRatio;
             this.camera.aspect = this.width / this.height;
             this.renderer.setSize(this.width, this.height);
             this.camera.updateProjectionMatrix();
         }
     }
 
-    componentDidMount(){        
+    componentDidMount() {
         this.init();
     }
 
-    render(){
+    render() {
         return (
             <Layout title={"Camera Position Demo"}>
-                <div  ref={this.containerRef}>
-                    <canvas id="canvas3d" className="absoluteTopLeftZeroW100H100" width={this.width} height={this.height}></canvas>
-                </div>                
+                <div ref={this.containerRef}>
+                    <canvas ref={this.canvasRef} id="canvas3d" className="absoluteTopLeftZeroW100H100" width={this.width} height={this.height}></canvas>
+                </div>
             </Layout>
         );
     }
